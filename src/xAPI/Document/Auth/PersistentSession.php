b34e6edb883e845eeb45c9ce6bb2dac7c17da8bc
Jakob Murko <jakob.murko@gmail.com>
2016-12-02 03:45:59 +0100

    Update copyright

--- src/xAPI/Document/Auth/PersistentSession.php
+++ src/xAPI/Document/Auth/PersistentSession.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of lxHive LRS - http://lxhive.org/
  *
- * Copyright (C) 2015 Brightcookie Pty Ltd
+ * Copyright (C) 2016 Brightcookie Pty Ltd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
