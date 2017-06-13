3601a2c6a8fcc305b27ec92b3a912301d8c8c447
Joerg Boeselt <joerg@brightcookie.com.au>
2017-06-13 17:46:34 +0930

    report command formatting

--- src/xAPI/Console/LrsReportCommand.php
+++ src/xAPI/Console/LrsReportCommand.php
@@ -36,6 +36,9 @@ use API\Admin\LrsReport;
 
 class LrsReportCommand extends Command
 {
+    /**
+     * @inheritDoc
+     */
     protected function configure()
     {
         $this
@@ -44,6 +47,9 @@ class LrsReportCommand extends Command
         ;
     }
 
+    /**
+     * @inheritDoc
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $report = new LrsReport();
@@ -53,7 +59,6 @@ class LrsReportCommand extends Command
         $table->setHeaders([
             'item', 'status', 'message', 'notes'
         ]);
-
         foreach($result as $title => $section){
             $this->renderTableSection($title, $section, $output, $table);
         }
@@ -61,19 +66,62 @@ class LrsReportCommand extends Command
         $table->render();
     }
 
-    protected function renderTableSection($title, $result, OutputInterface $output, Table $table)
+    /**
+     * Render a report section into a Symfony console table
+     * @see LrsReport::check()
+     *
+     * @param string $caption section title
+     * @param string $result LrsReport::check() result item
+     * @param OutputInterface $output
+     * @param Table $table
+     *
+     * @return void
+     */
+    protected function renderTableSection($caption, $result, OutputInterface $output, Table $table)
     {
         $table->addRow(new TableSeparator());
-        $table->addRow(['<info>'.$title.'</info>']);
+        $table->addRow([
+            $this->style('caption', $caption)
+        ]);
         $table->addRow(new TableSeparator());
+
         foreach($result as $item => $data){
             $table->addRow([
                 $item,
-                $data['status'],
+                $this->style($data['status'], $data['status']),
                 $data['value'],
                 $data['note'],
             ]);
         }
+    }
+
+    /**
+     * Styles a console message
+     * @see http://symfony.com/doc/current/console/coloring.html
+     *
+     * @param string $status see \API\Admin\LrsReport
+     * @param string $message
+     * @return string
+     */
+    protected function style($status, $message)
+    {
+
+        switch ($status) {
+            case 'caption': {
+                return '<fg=cyan;options=bold>'.$message.'</>';
+            }
+            case 'success': {
+                return '<fg=green;options=bold>'.$message.'</>';
+            }
+            case 'error': {
+                return '<fg=red;options=bold>'.$message.'</>';
+            }
+            case 'warn': {
+                return '<fg=yellow>'.$message.'</>';
+            }
+        }
+
+        return $message;
 
     }
 }
