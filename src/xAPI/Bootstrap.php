3bb3167e0fe0cc66dc8758ed80b338fa2026b328
Jakob Murko <jakob.murko@gmail.com>
2017-07-26 00:48:28 +0200

    Add more allowed headers to CORS request

--- src/xAPI/Bootstrap.php
+++ src/xAPI/Bootstrap.php
@@ -517,7 +517,7 @@ class Bootstrap
                 // Remove body, add headers
                 $parameters->remove('content');
                 // TODO: Allow more headers here
-                $allowedHeaders = ['content-type', 'authorization'];
+                $allowedHeaders = ['content-type', 'authorization', 'x-experience-api-version', 'content-length'];
                 foreach ($parameters as $key => $value) {
                     if (in_array(strtolower($key), $allowedHeaders)) {
                         $request = $request->withHeader($key, explode(',', $value));
